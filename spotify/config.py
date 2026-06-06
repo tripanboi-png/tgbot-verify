8cab5d22460e6b147e2f941e4e2898bd1d3dae8f
tripanboi-png <tripanboi@gmail.com>
2026-06-07 05:41:16 +0700

    Update config.py

--- spotify/config.py
+++ spotify/config.py
@@ -1,7 +1,7 @@
 # SheerID 验证配置文件
 
 # SheerID API 配置
-PROGRAM_ID = '67c8c14f5f17a83b745e3f82'
+PROGRAM_ID = '63fd266996552d469aea40e1'
 SHEERID_BASE_URL = 'https://services.sheerid.com'
 MY_SHEERID_URL = 'https://my.sheerid.com'
 
